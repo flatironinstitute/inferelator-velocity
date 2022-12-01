4ff53f35f77ba8b2781ea65c45ce7b178cd1cb49
asistradition <asistradition@users.noreply.github.com>
2022-12-01 15:16:32 -0500

    Add cumulative variance plotting tool

--- inferelator_velocity/plotting/__init__.py
+++ inferelator_velocity/plotting/__init__.py
@@ -1,1 +1,8 @@
-from .program_times import program_time_summary
+from .program_times import (
+    program_time_summary
+)
+
+from .mcv_summary import (
+    mcv_plot,
+    cumulative_variance_plot
+)
--- inferelator_velocity/plotting/mcv_summary.py
+++ inferelator_velocity/plotting/mcv_summary.py
@@ -11,7 +11,8 @@ from ..utils.keys import (
 def mcv_plot(
     adata,
     program=None,
-    ax=None
+    ax=None,
+    add_labels=True
 ):
     """
     Generate a summary figure for a specific program from an AnnData object
@@ -22,6 +23,8 @@ def mcv_plot(
     :type program: str
     :param ax: Matplotlib axis to draw into
     :type ax: Axes, optional
+    :param add_labels: Add labels to axes
+    :type add_labels: bool
     :return: Returns figure & axes objects if `ax=None` was passed, otherwise
         returns axes
     :rtype: matplotlib.Figure (optional), dict[Axes]
@@ -59,19 +62,101 @@ def mcv_plot(
         linestyle="--"
     )
 
-    ax.set_ylabel(
-        "MSE"
-    )
+    if add_labels:
+        ax.set_ylabel(
+            "MSE"
+        )
 
-    ax.set_xlabel(
-        "# Components"
-    )
+        ax.set_xlabel(
+            "# Components"
+        )
 
     ax.annotate(
-        f"{min_comp} Components",
+        f"{min_comp} Comps.",
         xy=(0.2, 0.8),
         xycoords='axes fraction'
     )
+
+    if fig is None:
+        return ax
+    else:
+        return fig, ax
+
+
+def cumulative_variance_plot(
+    adata,
+    program=None,
+    ax=None,
+    add_labels=True
+):
+    """
+    Generate a summary figure for a specific program from an AnnData object
+
+    :param adata: Anndata object which `ifv.program_times()` has been called on
+    :type adata: ad.AnnData
+    :param program: Program name
+    :type program: str
+    :param ax: Matplotlib axis to draw into
+    :type ax: Axes, optional
+    :param add_labels: Add labels to axes
+    :type add_labels: bool
+    :return: Returns figure & axes objects if `ax=None` was passed, otherwise
+        returns axes
+    :rtype: matplotlib.Figure (optional), dict[Axes]
+    """
+
+    # Create a figure if needed
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=(3, 3), dpi=300)
+
+    else:
+        fig = None
+
+    # Get the data from the program assignment .uns key
+    if program is None:
+
+        try:
+            data = adata.uns['pca']
+        except KeyError:
+            raise RuntimeError(
+                "PCA not found. "
+                "Run sc.pp.pca() "
+                "on this object before plotting"
+            )
+
+        try:
+            n = adata.uns[PROGRAM_KEY]['n_comps']
+        except KeyError:
+            raise RuntimeError(
+                "Program information not found. "
+                "Run ifv.assign_genes_to_programs()"
+                "on this object before plotting"
+            )
+
+    # Or a program-specific time .uns key
+    else:
+        data = adata.uns[OBSM_PCA_KEY.format(prog=program)]
+        n = data['variance_ratio'].shape[0]
+
+    data = np.cumsum(data['variance_ratio'].ravel()[:n]) * 100
+
+    ax.plot(
+        np.arange(n + 1),
+        np.insert(data, 0, 0),
+        color="gray"
+    )
+
+    if add_labels:
+
+        ax.set_ylabel(
+            "% Variance"
+        )
+
+        ax.set_xlabel(
+            "# Components"
+        )
+
+    ax.set_ylim(0., None)
 
     if fig is None:
         return ax
