Fix error in time calculation

inferelator_velocity/times.py
@@ -293,7 +293,7 @@ def calculate_times(
     }
 
     print(centroid_lookup)
-
+    print(cluster_order_dict)
     # Generate times for all observations against all splines
     all_times = np.hstack([
         _time_projection(
@@ -306,7 +306,7 @@ def calculate_times(
         ).reshape(-1, 1)
         for start, (end, l_time, r_time) in cluster_order_dict.items()
     ])
-
+    print(all_times)
     # Build a lookup table for each spline
     spline_lookup = {
         (start, end): (i, centroids[start], centroids[end])
@@ -497,7 +497,7 @@ def _time_projection(
         x,
         start_position,
         end_position,
-    ) * (end_time - start_time) + end_time
+    ) * (end_time - start_time) + start_time
 
     if wrap_time is not None:
         time = _wrap_time(time, wrap_time)